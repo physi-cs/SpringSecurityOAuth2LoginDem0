f5c9a4a44ce670e1bee34e10b523f0a892471bd3
hezhan <736097782@qq.com>
2021-01-26 09:58:54 +0800

    1.改为通过oauth/v20/me接口获取granted用户信息 2.修给EntityConverter，在get请求添加queryparam

--- src/main/java/sample/custom/CustomEntityConverter.java
+++ src/main/java/sample/custom/CustomEntityConverter.java
@@ -8,9 +8,6 @@ import org.springframework.http.RequestEntity;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.AuthenticationMethod;
-import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -18,46 +15,45 @@ import java.util.Collections;
 
 public class CustomEntityConverter implements Converter<OAuth2UserRequest, RequestEntity<?>> {
 
-	private static final MediaType DEFAULT_CONTENT_TYPE = MediaType
-			.valueOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
+    private static final MediaType DEFAULT_CONTENT_TYPE = MediaType
+            .valueOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
 
-	/**
-	 * Returns the {@link RequestEntity} used for the UserInfo Request.
-	 * @param userRequest the user request
-	 * @return the {@link RequestEntity} used for the UserInfo Request
-	 */
-	@Override
-	public RequestEntity<?> convert(OAuth2UserRequest userRequest) {
-		ClientRegistration clientRegistration = userRequest.getClientRegistration();
-		HttpMethod httpMethod = getHttpMethod(clientRegistration);
-		HttpHeaders headers = new HttpHeaders();
-		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-		URI uri = UriComponentsBuilder
-				.fromUriString(clientRegistration.getProviderDetails().getUserInfoEndpoint().getUri()).build().toUri();
+    /**
+     * Returns the {@link RequestEntity} used for the UserInfo Request.
+     *
+     * @param userRequest the user request
+     * @return the {@link RequestEntity} used for the UserInfo Request
+     */
+    @Override
+    public RequestEntity<?> convert(OAuth2UserRequest userRequest) {
+        ClientRegistration clientRegistration = userRequest.getClientRegistration();
+        HttpMethod httpMethod = getHttpMethod(clientRegistration);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        String tokenValue = userRequest.getAccessToken().getTokenValue();
+        URI uri = UriComponentsBuilder
+                .fromUriString(clientRegistration.getProviderDetails().getUserInfoEndpoint().getUri()).queryParam("access_token", tokenValue).build().toUri();
+        RequestEntity<?> request = new RequestEntity<>(headers, httpMethod, uri);
+        //if (HttpMethod.POST.equals(httpMethod)) {
+        //	headers.setContentType(DEFAULT_CONTENT_TYPE);
+        //	MultiValueMap<String, String> formParameters = new LinkedMultiValueMap<>();
+        //	formParameters.add(OAuth2ParameterNames.ACCESS_TOKEN, userRequest.getAccessToken().getTokenValue());
+        //	request = new RequestEntity<>(formParameters, headers, httpMethod, uri);
+        //}
+        //else {
+        //http标准的Bearer前缀
+        //  headers.setBearerAuth(userRequest.getAccessToken().getTokenValue());
+        //	request = new RequestEntity<>(headers, httpMethod, uri);
+        //}
+        return request;
+    }
 
-		RequestEntity<?> request;
-		if (HttpMethod.POST.equals(httpMethod)) {
-			headers.setContentType(DEFAULT_CONTENT_TYPE);
-			MultiValueMap<String, String> formParameters = new LinkedMultiValueMap<>();
-			formParameters.add(OAuth2ParameterNames.ACCESS_TOKEN, userRequest.getAccessToken().getTokenValue());
-			request = new RequestEntity<>(formParameters, headers, httpMethod, uri);
-		}
-		else {
-			//为了去除http标准的Bearer前缀
-			//headers.setBearerAuth(userRequest.getAccessToken().getTokenValue());
-			headers.set("Authorization",userRequest.getAccessToken().getTokenValue());
-			request = new RequestEntity<>(headers, httpMethod, uri);
-		}
-
-		return request;
-	}
-
-	private HttpMethod getHttpMethod(ClientRegistration clientRegistration) {
-		if (AuthenticationMethod.FORM
-				.equals(clientRegistration.getProviderDetails().getUserInfoEndpoint().getAuthenticationMethod())) {
-			return HttpMethod.POST;
-		}
-		return HttpMethod.GET;
-	}
+    private HttpMethod getHttpMethod(ClientRegistration clientRegistration) {
+        if (AuthenticationMethod.FORM
+                .equals(clientRegistration.getProviderDetails().getUserInfoEndpoint().getAuthenticationMethod())) {
+            return HttpMethod.POST;
+        }
+        return HttpMethod.GET;
+    }
 
 }
